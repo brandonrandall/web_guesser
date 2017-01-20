c456b1fc956600c7d352e6c4793efa601c2fcb63
BRANDON RANDALL <brandon1985@BRANDONs-MacBook-Pro.local>
2017-01-19 22:51:37 -0700

    web guesser no extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,10 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+SECRET_NUMBER = rand(100)
 
 get '/' do
+  guess = params["guess"].to_i
+  message = check_guess(guess)
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :guess => guess}
 
-  erb :index, :locals => {:number => number}
+end
 
+def check_guess(guess)
+  if guess > SECRET_NUMBER
+    message = "Too high!"
+  elsif guess < SECRET_NUMBER
+    message = "Way too low!"
+  else guess == SECRET_NUMBER
+    message = "#{SECRET_NUMBER}"
+  end
 end
