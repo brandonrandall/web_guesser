72f807cd39db645574b9b1a96bf4f7485c020aef
BRANDON RANDALL <brandon1985@BRANDONs-MacBook-Pro.local>
2017-01-21 12:40:18 -0700

    handles use case where guess doesn't exist
    Peviously code attempted to convert absent guesses (nil) to_i
    but this conversion set guess to 0, causing unintended behavior.
    These changes keep enforce that absent guesses remain nil.

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,19 +1,53 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'pry'
+
 
 SECRET_NUMBER = rand(100)
 
 get '/' do
-  guess = params["guess"].to_i
+  guess = params["guess"].to_i unless params["guess"] == nil
+  # in other words...
+  # if !params["guess"]
+  #   guess = nil
+  # else
+  #   guess = params["guess"].to_i
+  # end
+
   message = check_guess(guess)
   erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :guess => guess}
-
 end
 
 def check_guess(guess)
-  if guess > SECRET_NUMBER
-    message = "Too high!"
-  elsif guess < SECRET_NUMBER
+  analyze(guess)
+end
+
+def analyze(guess)
+  if !guess
+    message = "Enter a guess"
+  elsif guess > SECRET_NUMBER + 5
+    message = "Way too high!"
+  elsif guess == SECRET_NUMBER + 5
+    message = "Too high"
+  elsif guess == SECRET_NUMBER + 4
+    message = "Too high"
+  elsif guess == SECRET_NUMBER + 3
+    message = "Too high"
+  elsif guess == SECRET_NUMBER + 2
+    message = "Too high"
+  elsif guess == SECRET_NUMBER + 1
+    message = "Too high"
+  elsif guess == SECRET_NUMBER - 1
+    message = "Too low"
+  elsif guess == SECRET_NUMBER - 2
+    message = "Too low"
+  elsif guess == SECRET_NUMBER - 3
+    message = "Too low"
+  elsif guess == SECRET_NUMBER - 4
+    message = "Too low"
+  elsif guess == SECRET_NUMBER - 5
+    message = "Too low"
+  elsif guess < SECRET_NUMBER - 5
     message = "Way too low!"
   else guess == SECRET_NUMBER
     message = "#{SECRET_NUMBER}"
